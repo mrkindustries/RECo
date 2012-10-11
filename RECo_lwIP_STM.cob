e18e439f6a7dee047bce8ffb7125b49523e6f8e9
Marcos Chaparro <marcos@mrkindustries.com.ar>
2012-10-10 21:57:27 -0300

    First commit

---
+++ RECo_lwIP_STM.cob
@@ -0,0 +1,71 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<Project>
+  <Device>
+    <Manufacturer id="ST" name="ST"/>
+    <Chip id="STM32F107VC" name="STM32F107VC"/>
+  </Device>
+  <Components>
+    <Component id="c37a9351-96ed-11df-80ae-001d7d723e56" name="C Library"/>
+    <Component id="c37f2731-96ed-11df-80ae-001d7d723e56" name="Retarget printf"/>
+    <Component id="bf7c3c91-96ed-11df-80ae-001d7d723e56" name="CMSIS core"/>
+    <Component id="bfa102a1-96ed-11df-80ae-001d7d723e56" name="RCC"/>
+    <Component id="bfe42621-96ed-11df-80ae-001d7d723e56" name="GPIO"/>
+    <Component id="bff208d1-96ed-11df-80ae-001d7d723e56" name="EXTI"/>
+    <Component id="c0446e91-96ed-11df-80ae-001d7d723e56" name="SPI"/>
+    <Component id="c04d9651-96ed-11df-80ae-001d7d723e56" name="I2C"/>
+    <Component id="c05cffa2-96ed-11df-80ae-001d7d723e56" name="FLASH"/>
+    <Component id="c064a0c1-96ed-11df-80ae-001d7d723e56" name="TIM"/>
+    <Component id="c0725c61-96ed-11df-80ae-001d7d723e56" name="ADC"/>
+    <Component id="c3dde901-96ed-11df-80ae-001d7d723e56" name="USART"/>
+    <Component id="bdd41990-4e02-11e0-8ec0-001d7dcd0baf" name="CMSIS Boot"/>
+    <Component id="64d2c4e0-4e11-11e0-8ec0-001d7dcd0baf" name="MISC"/>
+  </Components>
+  <ExcludedFiles>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/lwIP/src/netif/ethernetif.c"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/fatFs/diskio.c"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/fatFs/diskio.h"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/fatFs/ff.c"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/fatFs/ff.h"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/fatFs/ffconf.h"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/fatFs/integer.h"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/fatFs/option/cc932.c"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/fatFs/option/cc936.c"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/fatFs/option/cc949.c"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/fatFs/option/cc950.c"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/fatFs/option/ccsbcs.c"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/fatFs/option/syscall.c"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/lwIP/src/include/ipv6/lwip/icmp.h"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/lwIP/src/include/ipv6/lwip/inet.h"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/lwIP/src/include/ipv6/lwip/ip.h"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/lwIP/src/include/ipv6/lwip/ip_addr.h"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/lwIP/port/tftpserver.c"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/lwIP/port/tftpserver.h"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/lwIP/port/tftputils.c"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/lwIP/port/tftputils.h"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/lwIP/port/arch/junk/init.h"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/lwIP/port/arch/junk/lib.h"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/lwIP/port/arch/junk/sys_arch.c"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/lwIP/port/arch/junk/sys_arch.h"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/lwIP/port/client.c"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/projCfg/stm32_cfg/stm32f10x_it1.c"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/projCfg/stm32_cfg/stm32f10x_it1.h"/>
+    <Exclude path="C:/CooCox/CoIDE/workspace/RECo_lwIP/lwIP/port/fsdata.c"/>
+    <Exclude path="Utilities/lwip-1.3.1/src/netif/ethernetif.c"/>
+    <Exclude path="Utilities/lwip-1.3.1/src/include/ipv6/lwip/icmp.h"/>
+    <Exclude path="Utilities/lwip-1.3.1/src/include/ipv6/lwip/inet.h"/>
+    <Exclude path="Utilities/lwip-1.3.1/src/include/ipv6/lwip/ip.h"/>
+    <Exclude path="Utilities/lwip-1.3.1/src/include/ipv6/lwip/ip_addr.h"/>
+    <Exclude path="Utilities/lwip-1.3.1/port/arch/junk/init.h"/>
+    <Exclude path="Utilities/lwip-1.3.1/port/arch/junk/lib.h"/>
+    <Exclude path="Utilities/lwip-1.3.1/port/arch/junk/sys_arch.c"/>
+    <Exclude path="Utilities/lwip-1.3.1/port/arch/junk/sys_arch.h"/>
+    <Exclude path="Libraries/CMSIS/Core/CM3/startup/gcc/startup_stm32f10x_cl.s"/>
+    <Exclude path="Libraries/CMSIS/Core/CM3/startup/gcc/startup_stm32f10x_hd.s"/>
+    <Exclude path="Libraries/CMSIS/Core/CM3/startup/gcc/startup_stm32f10x_ld.s"/>
+    <Exclude path="Libraries/CMSIS/Core/CM3/startup/gcc/startup_stm32f10x_md.s"/>
+    <Exclude path="Utilities/lwip-1.3.1/port/fsdata.c"/>
+    <Exclude path="Utilities/STM32_EVAL/STM3210C_EVAL/stm3210c_eval_lcd.c"/>
+    <Exclude path="Utilities/STM32_EVAL/STM3210C_EVAL/stm3210c_eval_lcd.h"/>
+  </ExcludedFiles>
+  <Drivers/>
+</Project>
